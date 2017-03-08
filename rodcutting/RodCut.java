9e3152ec8296bbfea47f0f220b85ee9a0773718e
pedro <p.simoes@campus.fct.unl.pt>
2017-03-08 17:55:32 +0000

    bottum-up-cut-rod added

--- rodcutting/RodCut.java
+++ rodcutting/RodCut.java
@@ -12,6 +12,8 @@ public class RodCut {
                             cut_rod(arr, size));
         System.out.println("Maximum Obtainable Value with dynamic is "+
                 cut_rod_dynamic(arr, size));
+        System.out.println("Maximum Obtainable Value with dynamic is "+
+        		bottum_up_cut_rod(arr, size));
 
 	}
 	//input array of prices and a length of the rope
@@ -47,6 +49,23 @@ public class RodCut {
 		}
 		saved.put(n, q);
 		return q;
+	}
+	static int bottum_up_cut_rod(int[]p,int n){
+		
+		int val[] = new int[n+1];
+        val[0] = 0;
+ 
+        // Build the table val[] in bottom up manner and return
+        // the last entry from the table
+        for (int i = 1; i<=n; i++)
+        {
+            int max_val = Integer.MIN_VALUE;
+            for (int j = 0; j < i; j++)
+                max_val = Math.max(max_val,p[j] + val[i-j-1]);
+            val[i] = max_val;
+        }
+ 
+        return val[n];
 	}
 	
 }
