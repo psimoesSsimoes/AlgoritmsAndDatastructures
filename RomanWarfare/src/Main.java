62533a9dd0bc967eab41bc80a1447bc5ffd565d0
pedro <p.simoes@campus.fct.unl.pt>
2017-03-25 23:24:41 +0000

    need to find a way to check if i use an army, there are enough armies left to attack all the cities

--- RomanWarfare/src/Main.java
+++ RomanWarfare/src/Main.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
+import Models.FakeTuple;
 import Models.RomanWarfareSolver;
+
 
 
 public class Main {
@@ -30,37 +32,56 @@ public class Main {
 		
 		int nArmies = Integer.parseInt(ap.split(" ")[0]);
 		int nCities = Integer.parseInt(ap.split(" ")[1]);
-		x_a = new int[nArmies+2];
-		y_a = new int[nArmies+2];
-		y_c = new int[nCities+2];
-		x_c = new int[nCities+2];
-		int[] m = new int[nArmies+1];
-		int[] w = new int[nCities+1];
+		x_a = new int[nArmies+1];
+		y_a = new int[nArmies+1];
+		y_c = new int[nCities+1];
+		x_c = new int[nCities+1];
+		int[] bs = new int[nCities+1];
 		int[][] a_c = new int[nArmies+2][nCities+2];
-		
-		for(int i = 2; i < nArmies+2; i++){
+		FakeTuple[] a = new FakeTuple[nArmies+1];
+		for(int i = 1; i < nArmies+1; i++){
 			String aA = buf.readLine();
 			String[] aAv = aA.split(" ");
 			x_a[i]=Integer.parseInt(aAv[0]);
 			y_a[i]=Integer.parseInt(aAv[1]);
-			a_c[i][0]= Integer.parseInt(aAv[2]);//keep maintance values on colum 0
-			a_c[i][1] = i;//keep index for manhatan coord on colum 1
-			print(Arrays.toString(x_a));
+			a_c[i][0]= Integer.parseInt(aAv[2]);//
+			
 		}
 		
-		for(int j = 2; j < nCities+2; j++){
+		for(int j = 1; j < nCities+1; j++){
 			String aC = buf.readLine();
 			String[] aCv = aC.split(" ");
 			x_c[j]=Integer.parseInt(aCv[0]);
 			y_c[j]=Integer.parseInt(aCv[1]);
 			a_c[0][j]=Integer.parseInt(aCv[2]);//keep wealth values on row 0
-			a_c[1][j] = j;//keep index for manhatan coord on colum 1
 			print(Arrays.toString(x_c));
 		}
 		
-		int min = Integer.MAX_VALUE;
-		int previous_value;
 		
+		//minDistance to from army to city
+		int mD=Integer.MAX_VALUE;
+		int maxR=0,minD=0,minC=0;
+		int lastM=Integer.MAX_VALUE;
+		int tD;
+		int tC=Integer.MAX_VALUE;
+		int ttD=Integer.MAX_VALUE;
+		for(int ct = 1 ; ct < nCities+1 ; ct++){
+			for (int ar = 1; ar < nArmies+1; ar++){
+				if(a_c[ar][0] >= a_c[0][ct] && (ttD = Math.min(dist(ar,ct),mD))<=mD ){
+					if(ttD==mD){
+						if(tC > a_c[ar][0]){
+							
+						}
+						
+					}else{
+						mD=ttD;
+					}
+				}
+			}
+			maxR+=a_c[0][ct];
+			minD+=mD
+			
+		}
 		printGrid(a_c);
 		int[] cty = new int[nCities];
 		
@@ -70,14 +91,27 @@ public class Main {
 		
 		return Math.abs(x_a[i]-x_c[c]) + Math.abs(y_a[i]-y_c[c]);
 	}
+	private static char min(int f,int s){
+		if(f<s)
+			return 'f';
+		else if(f > s)
+			return 's';
+		else
+			return 't';
+		
+	}
 	public static void print(String x){
 		System.out.println(x);
 	}
 	public static void printGrid(int[][] a)
 	{
-	   for(int i = 0; i < 5; i++)
+		/**
+		 * int rows = matrix.length;
+int cols = matrix[0].length;
+		 */
+	   for(int i = 0; i < a.length; i++)
 	   {
-	      for(int j = 0; j < 4; j++)
+	      for(int j = 0; j < a[0].length; j++)
 	      {
 	         System.out.printf("%5d ", a[i][j]);
 	      }
---
+++ RomanWarfare/src/Models/FakeTuple.java
@@ -0,0 +1,23 @@
+package Models;
+
+public class FakeTuple {
+	public final int x;
+	public final int y;
+
+
+	public FakeTuple(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+
+
+}
