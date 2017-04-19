77b988d255315e57143ef00023399336205d76e7
pedro <p.simoes@campus.fct.unl.pt>
2017-04-19 17:53:44 +0100

    matrix is not taking me anywhere

--- HardWeeks/src/HardWeeksSolver.java
+++ HardWeeks/src/HardWeeksSolver.java
@@ -1,18 +1,78 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Stream;
 
 public class HardWeeksSolver {
 
-	public HardWeeksSolver(int[][] adj_matrix) {
-		int[][] matrix = adj_matrix;
+	private int[][] matrix;
+	private boolean[] control;
+	private int limit;
+	private int maxWeek;
+	private int nHardWeeks=0;
+
+	public HardWeeksSolver(int[][] adj_matrix, boolean[] control, int l) {
+		this.matrix = adj_matrix;
+		this.control = control;
+		this.limit = l;
 	}
 
+	/**
+	 * first case looks for false in colums. from there calculates hard weeks.
+	 */
 	public void solve() {
+		Queue<Integer> startingPoint = findBeginning();
+
+		if (startingPoint.size() > limit) {
+			maxWeek = startingPoint.size();
+			nHardWeeks += 1;
+		}
+		while (!startingPoint.isEmpty()) {
+			Queue<Integer> week = new LinkedList<Integer>();
+			while (!startingPoint.isEmpty()) {
+				int row = startingPoint.poll();
+				for (int j = 0; j < control.length; j++) {
+					if(matrix[row][j]==1)
+					if (isLonelyCollum(row, j)) {
+						matrix[row][j]=0;
+						week.add(j);
+					}
+				}
+			}
+			if (week.size() > limit )
+				nHardWeeks++;
+			if (maxWeek < week.size()) {
+				maxWeek = week.size();
+			}
+			startingPoint.addAll(week);
+			week.forEach(z->System.out.print(z));
+		}
 		
-		
+	}
+
+	private Queue<Integer> findBeginning() {
+		Queue<Integer> q = new LinkedList<Integer>();
+		for (int i = 0; i < control.length; i++) {
+			if (control[i] == false)
+				q.add(i);
+		}
+		return q;
+	}
+
+	private boolean isLonelyCollum(int row, int colum) {
+		int i = 0;
+		for (int f = 0; f < control.length; f++) {
+			i += matrix[f][colum];
+		}
+		if (i == 1)
+			return true;
+		else
+			return false;
 	}
 
 	public String answer() {
 		// TODO Auto-generated method stub
-		return null;
+		return maxWeek + " "+ nHardWeeks;
 	}
 
 }
--- HardWeeks/src/Main.java
+++ HardWeeks/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,7 +10,71 @@ public class Main {
 	 * @param args
 	 * @throws IOException 
 	 */
+	private static final String FILENAME = "/home/psimoes/gitlab/ADA/HardWeeks/hardtest.txt";
 	public static void main(String[] args) throws IOException {
+		
+		
+		
+		
+		
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+
+
+			br = new BufferedReader(new FileReader(FILENAME));
+			String[] input = br.readLine().split(" ");
+			
+			int t = Integer.parseInt(input[0]);
+			int p = Integer.parseInt(input[1]);
+			int l = Integer.parseInt(input[2]);
+			
+			int[][] adj_matrix = new int[t][t];
+			
+			boolean[] control = new boolean[t]; 
+			//as it receives input builds the adjacentes matrix and checks on control the colums occupied 
+			for(int i = 0 ; i< p ;i++){
+				String[] precedences = br.readLine().split(" "); 
+				int ptask = Integer.parseInt(precedences[0]);
+				int atask = Integer.parseInt(precedences[1]);
+				adj_matrix[ptask][atask]=1;
+				control[atask]=true;
+			}
+			HardWeeksSolver slv = new HardWeeksSolver(adj_matrix,control,l);
+			slv.solve();
+			print(slv.answer());
+			print("Finished!");
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+		
+		
+		
+		
+		
 		
 		
 		BufferedReader buf = new BufferedReader(
@@ -17,23 +82,9 @@ public class Main {
 		
 		String[] input = buf.readLine().split(" ");
 		
-		int t = Integer.parseInt(input[0]);
-		int p = Integer.parseInt(input[1]);
-		int l = Integer.parseInt(input[2]);
-		
-		int[][] adj_matrix = new int[t][t];
 		
 		
-		for(int i = 0 ; i< t ;i++){
-			String[] precedences = buf.readLine().split(" "); 
-			int ptask = Integer.parseInt(precedences[0]);
-			int atask = Integer.parseInt(precedences[0]);
-			adj_matrix[ptask][atask]=1;
-		}
 		
-		HardWeeksSolver slv = new HardWeeksSolver(adj_matrix);
-		slv.solve();
-		print(slv.answer());
 		
 		buf.close();
 
