6ef1d1848c2a252ac1e1b118dcea01c8f3517c58
root <p.simoes@campus.fct.unl.pt>
2017-04-24 14:01:32 -0500

    promotions done but not tested. logic is the following. DAG with V nodes and E edges. N promotions. Employee x has no chance of being promoted if the number of predecessors is at least Pred(x)>=N. Employee will certainly be promoted if number of successors is at least Suc(x)>=V-N. For each employee compute Pred(x) and Suc(x)

--- Promotions/src/Main.java
+++ Promotions/src/Main.java
@@ -1,13 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Vector;
 
 public class Main {
 	private static final String FILENAME = "/root/workspace/Promotions/promotionsInput";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
-
+			Queue<Integer> e1 = new LinkedList<Integer>();
+			List<Queue<Integer>> a1 = new ArrayList<Queue<Integer>>(5001);
+			Queue<Integer> e2 = new LinkedList<Integer>();
+			List<Queue<Integer>> a2 = new ArrayList<Queue<Integer>>(5001);
 			String sCurrentLine;
 
 			String[] fLine = br.readLine().split(" ");
@@ -15,11 +23,18 @@ public class Main {
 			int B = Integer.parseInt(fLine[1]);
 			int N = Integer.parseInt(fLine[2]);
 			int P =Integer.parseInt(fLine[3]);
-			for(int i = 0; i< P;i++){
-				String[] precendences = br.readLine().split(" ");
+			
+			while(--P > 0){
+				String[] precedences = br.readLine().split(" ");
+				int x = Integer.parseInt(precedences[0]);
+				int y = Integer.parseInt(precedences[1]);
+				a1.get(x).offer(y);
+				a2.get(y).offer(x);
+				
 			}
 			
-			PromotionsSolver slv = new PromotionsSolver();
+			PromotionsSolver slv = new PromotionsSolver(a1,a2,N,A,B);
+			System.out.println(slv.answer());
 
 		} catch (IOException e) {
 			e.printStackTrace();
--- Promotions/src/PromotionsSolver.java
+++ Promotions/src/PromotionsSolver.java
@@ -1,4 +1,64 @@
+import java.util.List;
+import java.util.Queue;
+import java.util.Vector;
 
 public class PromotionsSolver {
+
+	
+	private int[] vis1,vis2,cnt1,cnt2= new int[5001];
+	private List<Queue<Integer>> a1;
+	private List<Queue<Integer>> a2;
+	private int n;
+	private int a;
+	private int b;
+	private String answer="";
+	
+	private void dfs1(int x) {
+		if (vis1[x]==1) return;
+		vis1[x] = 1;
+		cnt1[x]++;
+		for(int i = 0; i < a1.get(x).size(); i++) {
+			dfs1(a1.get(x).remove());
+		}
+	}
+
+	private void dfs2(int x) {
+		if (vis2[x]==1) return;
+		vis2[x] = 1;
+		cnt2[x]++;
+		for(int i = 0; i < a2.get(x).size(); i++) {
+			dfs1(a2.get(x).remove());
+		}
+	}
+	
+
+
+	public PromotionsSolver(List<Queue<Integer>> a1, List<Queue<Integer>> a2, int n, int a, int b) {
+		this.a1=a1;
+		this.a2=a2;
+		this.n=n;
+		this.a=a;
+		this.b=b;
+	}
+
+	public void solve() {
+		for(int i = 0; i< n;i++){
+			dfs1(i);
+			dfs2(i);
+		}
+		int ans1 = 0, ans2 = 0, ans3 = 0;
+		
+		for(int i = 0; i < n; i++) {
+			if(cnt1[i] > b) ans3++;
+			if(cnt2[i] > n - a) ans1++;
+			if(cnt2[i] > n - b) ans2++;
+		}
+		answer = ans1+" "+ans2+" "+ans3;
+		
+	}
+
+	public String answer() {
+		return answer;
+	}
 
 }
