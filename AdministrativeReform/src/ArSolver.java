e031a1cb93e76e599423440190201e4cc5e684bc
psimoes <p.simoes@campus.fct.unl.pt>
2017-05-07 15:09:34 +0100

    using adjacences list as matrix won't fit in memory

--- AdministrativeReform/src/ArSolver.java
+++ AdministrativeReform/src/ArSolver.java
@@ -1,9 +1,20 @@
 import java.util.Queue;
+import java.util.TreeMap;
 
 public class ArSolver {
 
-	public ArSolver(int communities, int roads, Queue<String[]> all_roads, String[] capitals) {
+	public ArSolver(int communities, int roads, TreeMap<Integer,TreeMap<Integer,Integer>> adj, String[] capitals) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public void solve() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getAnswer() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- AdministrativeReform/src/Main.java
+++ AdministrativeReform/src/Main.java
@@ -3,34 +3,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.TreeMap;
 
 public class Main {
 
+	private static TreeMap<Integer, TreeMap<Integer, Integer>> adj;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		BufferedReader br = null;
 		FileReader fr = null;
-		char[][] grid = new char[42][50];
+		adj = new TreeMap<Integer, TreeMap<Integer, Integer>>();
+
 		try {
 
 			String sCurrentLine;
 
 			br = new BufferedReader(new FileReader("/home/psimoes/test4.txt"));
-			
+
 			String[] c_r = br.readLine().split(" ");
 			int communities = Integer.parseInt(c_r[0]);
 			int roads = Integer.parseInt(c_r[1]);
 			Queue<String[]> all_roads = new LinkedList<String[]>();
-			for (int i = 0; i < roads; i++){
-				String[] road_x = br.readLine().split(" ");
-				all_roads.add(road_x);
+			for (int i = 0; i < roads; i++) {
+				String[] r = br.readLine().split(" ");
+				if (!adj.containsKey(Integer.parseInt(r[0]))) {
+					TreeMap<Integer, Integer> t = new TreeMap<Integer, Integer>();
+					t.put(Integer.parseInt(r[1]), Integer.parseInt(r[2]));
+					adj.put(Integer.parseInt(r[0]), t);
+				}
+				else{
+					adj.get(Integer.parseInt(r[0])).put(Integer.parseInt(r[1]), Integer.parseInt(r[2]));
+				}
 			}
 			String[] capitals = br.readLine().split(" ");
-			ArSolver slv = new ArSolver(communities,roads,all_roads,capitals);
-			
-			
-			
+			ArSolver slv = new ArSolver(communities, roads, adj, capitals);
+			slv.solve();
+			System.out.println(slv.getAnswer());
 			
 			
 			
@@ -58,7 +68,5 @@ public class Main {
 		}
 
 	}
-
-	
 
 }
